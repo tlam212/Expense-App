a36a2b26690b4459a9e364891f6eee9d0356b27c
Toni Lamont <tlam212@gmail.com>
2019-12-03 15:56:53 -0800

    updated account controller

--- expense_backend/app/controllers/api/v1/accounts_controller.rb
+++ expense_backend/app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::AccountsController < ApplicationController
+
+  def index
+
+    render json: Account.all
+  end
+
+  def show
+    render json: Account.find(params[:id])
+  end
+
+  def create
+    @account = Account.new(a_params)
+    if @account.save
+      render json: @account
+    else
+      render json: {error: "Please try again"}
+    end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+  end
+
+
+  private
+
+  def a_params
+    params.require(:account).permit(:name, :balance)
+  end
+
 end
--- expense_backend/app/models/account.rb
+++ expense_backend/app/models/account.rb
@@ -2,4 +2,39 @@ class Account < ApplicationRecord
   has_many :transactions
   validates :name, :balance, presence: true
 
+  def update_balance(transaction)
+    if transaction.type == "deposit"
+      self.balance = self.balance + transaction.amount
+      self.save
+    elsif
+      transaction.type == "withdraw"
+      if self.balance >= transaction.amount
+      self.balance = self.balance - transaction.amount
+      self.save
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
