0717bf5b490453fac2f27e86a4e951ae9445460c
Toni Lamont <tlam212@gmail.com>
2019-12-03 14:06:35 -0800

    backend api set up

--- expense_backend/app/models/account.rb
+++ expense_backend/app/models/account.rb
@@ -1,2 +1,5 @@
 class Account < ApplicationRecord
+  has_many :transactions
+  validates :amount, presence :true
+  validates_inclusion_of :type, :in => ["deposit", "withdraw"]
 end
--- expense_backend/config/routes.rb
+++ expense_backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  namesake :api
+  namesake :v1
   resources :transactions
   resources :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
